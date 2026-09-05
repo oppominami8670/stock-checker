cddb38569c436af7ce3ab440ac3d0b48d3d07c4a
oppominami8670 <oppominami8670@gmail.com>
2026-09-05 15:51:22 +0900

    feat: add offline service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'stock-checker-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(c => c.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
